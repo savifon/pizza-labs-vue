5d697ca1e54577d3f6c3a99daabb943d3f0e4f0c
Savio Fonseca <savifon1@gmail.com>
2022-03-10 13:53:26 -0300

    iniciando o app PizzaLabs em VueJS 3

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,109 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderApp :menu-options="menuOptions"/>
   <router-view/>
 </template>
 
+<script>
+import HeaderApp from '@/components/Header.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderApp
+  },
+  setup () {
+    const menuOptions = [
+      { title: 'Início', ref: '/' },
+      { title: 'Pedidos Realizados', ref: '/pedidos' }
+    ]
+
+    return {
+      menuOptions
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+
+:root {
+  --colorText: #333333;
+  --colorTextButton: #ffffff;
+  --colorPrimary: #e1301c;
+  --colorSecondary: #4f9844;
+  --colorBgPrimary: #f0f0f0;
+  --colorBgSecondary: #d3d3d3;
+  --radius: 10px;
 }
 
-nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+  color: var(--colorText);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a {
+  text-decoration: none;
+  color: #4f9844;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+button {
+  border: none;
+  background: var(--colorBgSecondary);
+  padding: 5px 10px;
+  border-radius: var(--radius);
+  color: var(--colorTextButton);
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid transparent;
+  transition: all ease .2s;
+
+  &.green {
+    background: var(--colorSecondary);
+  }
+
+  &.red {
+    background: var(--colorPrimary);
+  }
+
+  &.btn-checkout {
+    padding: 10px 15px;
+  }
+
+  &:disabled {
+    opacity: .2;
+  }
+
+  &:hover {
+    box-shadow: 0px 10px 10px -10px var(--colorText);
+    border-color: var(--colorTextButton);
+  }
+}
+
+div.container {
+  display: block;
+  margin: 0 auto;
+  padding: 50px 30px;
+  max-width: 1250px;
+
+  @media screen and (max-width: 424px) {
+    padding: 50px 15px;
+  }
+}
+
+div.flex-column {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 30px;
+
+  @media screen and (max-width: 767px) {
+    justify-content: center;
   }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="header">
+    <div class="box">
+      <a href="#" alt="Ir para o início">
+        <img :src="logo" alt="Pizza Labs" class="logo" />
+      </a>
+
+      <ul class="nav">
+        <li v-for="option in menuOptions" :key="option.ref">
+          <a :href="option.ref" :alt="option.title">
+            {{ option.title }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from '@/assets/images/logo.svg'
+
+export default {
+  name: 'HeaderApp',
+  props: {
+    menuOptions: {
+      type: Array,
+      required: true
+    }
+  },
+  setup () {
+    return {
+      logo
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+div.header {
+  box-shadow: 0px 5px 15px 1px var(--colorBgPrimary);
+  position: sticky;
+  top: 1px;
+  background: white;
+}
+
+div.box {
+  max-width: 1250px;
+  margin: 0 auto;
+  padding: 10px 30px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 15px;
+
+  @media screen and (max-width: 767px) {
+    justify-content: center;
+  }
+
+  & .logo {
+    max-height: 70px;
+    max-width: 100%;
+  }
+
+  & .nav {
+    display: flex;
+    gap: 10px;
+
+    & li {
+      transition: all ease 0.2s;
+      list-style: none;
+      display: flex;
+      border-radius: var(--radius);
+
+      & a {
+        padding: 10px;
+        color: var(--colorText);
+      }
+
+      &:hover {
+        background: var(--colorBgPrimary);
+      }
+    }
+  }
+}
+</style>
